c19c75b86ddd89f0f9fe5738063444fca57ba271
001anisa <anisakhan4635@gmail.com>
2022-11-16 06:08:38 +0530

    completed

--- calculate/src/App.js
+++ calculate/src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {useState} from 'react'
+function App() 
+{
+ //making state
+const [amount,setAmount] = useState('')
+const [perc,setPerc] = useState('')
+const [result,setResult] = useState(0)
 
-function App() {
+//Logic
+
+let calc = (e) =>
+{
+  e.preventDefault();
+  if(amount===0 || perc ===0)
+  {
+    alert("Please enter a valid Amount and Percentage")
+  }
+  else
+  {
+    let result=(amount*(perc/100))
+    setResult(result)
+  }
+
+}
+
+//reload
+let reload= () =>
+{
+  window.location.reload()
+}
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h2> Percentage Claculator</h2>
+        <form onSubmit={calc}>
+          <div>
+            <label>Amount</label>
+            <input type="text" placeholder="Enter amount" value={amount} onChange={(e)=> setAmount(e.target.value)}
+            />
+          </div>
+
+          <div>
+            <label>Percentage(%)</label>
+            <input type="text" placeholder="Enter percentage" value={perc} onChange={(e)=> setPerc(e.target.value)}/>
+          </div>
+
+          <div>
+            <button className="btn" type="submit" >
+              Claculate
+            </button>
+            <button className="reload" onClick={reload} type="submit">
+              Reload
+            </button>
+          </div>
+
+          <div className="center">
+           <p> <h2>Result :</h2>{result}</p>
+            
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
